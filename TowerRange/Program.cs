fadec36fdb265af795794ba23989ce2e24c050d7
Rulfy <git@zynox.net>
2015-02-27 15:04:01 +0300

    reworked  tower range

---
+++ TowerRange/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Ensage;
+using SharpDX;
+
+namespace TowerRange
+{
+    class Program
+    {
+        private const bool OwnTowers = true;
+        private const bool EnemyTowers = true;
+
+        static void Main(string[] args)
+        {
+            Drawing.OnEndScene += Drawing_OnEndScene;
+        }
+
+        static void Drawing_OnEndScene(EventArgs args)
+        {
+            if (Drawing.Direct3DDevice == null || Drawing.Direct3DDevice.IsDisposed)
+                return;
+
+            if (!OwnTowers && !EnemyTowers)
+                return;
+
+            var player = EntityList.GetLocalPlayer();
+            if (player == null )
+                return;
+            var towers = EntityList.GetEntities<Entity>().Where(x => x.Alive && x.ClassId == 323).ToList(); // TODO: add classes CDOTA_BaseNPC_Tower
+            if (!towers.Any())
+                return;
+
+            if (EnemyTowers)
+            {
+                foreach (var tower in towers.Where(x => x.Team != player.Team))
+                {
+                    Circle.DrawCircle(tower.Position, 850, Color.Red, 1);
+                }
+            }
+            if (OwnTowers)
+            {
+                foreach (var tower in towers.Where(x => x.Team == player.Team))
+                {
+                    Circle.DrawCircle(tower.Position, 850, Color.Green, 1);
+                }
+            }
+        }
+    }
+}
