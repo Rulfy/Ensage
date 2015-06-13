f74f2f542b7af57df4081b5e010435529f626298
Rulfy <git@zynox.net>
2015-06-13 15:45:45 +0300

    fix and cleanup for visible by enemy

--- VisibleByEnemy/Program.cs
+++ VisibleByEnemy/Program.cs
@@ -1,43 +1,60 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Ensage;
-using SharpDX;
 
 namespace VisibleByEnemy
 {
     class Program
     {
-        private static Dictionary<Entity,ParticleEffect> _effects = new Dictionary<Entity, ParticleEffect>();
+        private static readonly Dictionary<Hero, ParticleEffect> Effects = new Dictionary<Hero, ParticleEffect>();
         public static void Main(string[] args)
         {
             Entity.OnIntegerPropertyChange += Entity_OnIntegerPropertyChange;
+
+            // Initial Check
+            var player = EntityList.Player;
+            if (player == null)
+                return;
+
+            var heroes = EntityList.GetEntities<Hero>().Where(x => x.IsAlive && x.Team == player.Team);
+            foreach (var hero in heroes)
+            {
+                HandleEffect(hero);
+            }
         }
 
         static void Entity_OnIntegerPropertyChange(Entity sender, EntityIntegerPropertyChangeEventArgs args)
         {
+            if (sender == null)
+                return;
+
             var player = EntityList.Player;
-            if (player == null || sender == null)
+            if (player == null)
                 return;
 
             var hero = sender as Hero;
             if (hero != null && hero.Team == player.Team && args.Property == "m_iTaggedAsVisibleByTeam")
+                HandleEffect(hero);
+        }
+
+        static void HandleEffect(Hero hero)
+        {
+            if (hero.IsVisibleToEnemies)
             {
-                if (hero.IsVisibleToEnemies)
+                ParticleEffect effect;
+                if (!Effects.TryGetValue(hero, out effect))
                 {
-                    ParticleEffect effect;
-                    if (!_effects.TryGetValue(hero, out effect))
-                    {
-                        effect = hero.AddParticleEffect("aura_shivas");
-                        effect.SetVector(0, new Vector3(0, 0, 0));
-                    }
+                    effect = hero.AddParticleEffect("aura_shivas");
+                    Effects.Add(hero, effect);
                 }
-                else
+            }
+            else
+            {
+                ParticleEffect effect;
+                if (Effects.TryGetValue(hero, out effect))
                 {
-                    ParticleEffect effect;
-                    if (_effects.TryGetValue(hero, out effect))
-                    {
-                        effect.Dispose();
-                        _effects.Remove(sender);
-                    }
+                    effect.Dispose();
+                    Effects.Remove(hero);
                 }
             }
         }
