17a41897168330467f56fb3fd593a3ad6cadfc8e
Rulfy <git@zynox.net>
2015-11-17 15:05:18 +0100

    added menu to zoomhack and fixed glitches when zooming out too far

--- Zoomhack/Program.cs
+++ Zoomhack/Program.cs
@@ -1,29 +1,42 @@
-﻿using System;
-using System.Windows.Input;
-using Ensage;
+﻿using Ensage;
+using Ensage.Common.Menu;
 
 namespace Zoomhack
 {
 	class Program
 	{
-		// TODO: make config and set zoomhack on load
-		// TODO: make key configurable
+		private static readonly Menu Menu = new Menu("Zoomhack", "zoomhack", true);
+
 		private static readonly uint WM_MOUSEWHEEL = 0x020A;
-	    private static readonly uint VK_CTRL = 0x11;
-        private static readonly ConVar ZoomVar = Game.GetConsoleVar("dota_camera_distance");
+		private static readonly uint VK_CTRL = 0x11;
+		private static readonly ConVar ZoomVar = Game.GetConsoleVar("dota_camera_distance");
   
 		static void Main()
 		{
-            ZoomVar.RemoveFlags(ConVarFlags.Cheat);
-            Game.OnWndProc += Game_OnWndProc;
+			var slider = new MenuItem("distance", "Default camera distance").SetValue(new Slider(1134, 1134, 2500));
+			slider.ValueChanged += Slider_ValueChanged;
+			Menu.AddItem(slider);
+
+			Menu.AddToMainMenu();
+
+			ZoomVar.RemoveFlags(ConVarFlags.Cheat);
+			ZoomVar.SetValue(slider.GetValue<Slider>().Value);
+			Game.GetConsoleVar("r_farz").SetValue(18000);
+
+			Game.OnWndProc += Game_OnWndProc;
+		}
+
+		// ReSharper disable once InconsistentNaming
+		private static void Slider_ValueChanged(object sender, OnValueChangeEventArgs e)
+		{
+			ZoomVar.SetValue(e.GetNewValue<Slider>().Value);
 		}
 
 		private static void Game_OnWndProc(WndEventArgs args)
 		{
-            
-			if (args.Msg == WM_MOUSEWHEEL/* && Game.IsInGame*/ )
+			if (args.Msg == WM_MOUSEWHEEL && Game.IsInGame )
 			{
-                if (Game.IsKeyDown(VK_CTRL))
+				if (Game.IsKeyDown(VK_CTRL))
 				{
 					// Get HIWORD(wParam)
 					var delta = (short)((args.WParam >> 16) & 0xFFFF);
@@ -33,8 +46,8 @@ namespace Zoomhack
 						zoomValue += 50;
 					else
 						zoomValue -= 50;
-                    // Set updated value
-                    ZoomVar.SetValue(zoomValue);
+					// Set updated value
+					ZoomVar.SetValue(zoomValue);
 					// Block processed input from game
 					args.Process = false;
 				}
