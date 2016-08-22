c1e54b0375dc2d18c4bd1868c39e298f19454f55
Rulfy <git@zynox.net>
2016-08-23 00:08:12 +0200

    visiblebyenemy using the propertyupdate callback now

--- VisibleByEnemy/Program.cs
+++ VisibleByEnemy/Program.cs
@@ -1,29 +1,18 @@
+using System;
+
 namespace VisibleByEnemy
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-
     using Ensage;
-    using Ensage.Common;
     using Ensage.Common.Menu;
-    using Ensage.Common.Objects;
 
     internal class Program
     {
         #region Static Fields
 
-        private static readonly Dictionary<Unit, ParticleEffect> Effects = new Dictionary<Unit, ParticleEffect>();
+        private static Dictionary<Unit, ParticleEffect> _effects = new Dictionary<Unit, ParticleEffect>();
 
         private static readonly Menu Menu = new Menu("VisibleByEnemy", "visibleByEnemy", true);
-
-        private static List<Building> buildings = new List<Building>();
-
-        private static List<Unit> mines = new List<Unit>();
-
-        private static List<Unit> units = new List<Unit>();
-
-        private static List<Unit> wards = new List<Unit>();
 
         #endregion
 
@@ -52,169 +41,147 @@ namespace VisibleByEnemy
             Menu.AddItem(item);
 
             Menu.AddToMainMenu();
-
-            Game.OnIngameUpdate += Game_OnIngameUpdate;
+            Entity.OnInt32PropertyChange += Entity_OnInt32PropertyChange;
         }
 
         #endregion
 
         #region Methods
+        private static bool IsWard(Entity sender)
+        {
+            return (sender.ClassID == ClassID.CDOTA_NPC_Observer_Ward ||
+                    sender.ClassID == ClassID.CDOTA_NPC_Observer_Ward_TrueSight);
+        }
 
-        private static void HandleEffect(Unit unit)
+        private static bool IsMine(Entity sender)
+        {
+            return sender.ClassID == ClassID.CDOTA_NPC_TechiesMines;
+        }
+
+        private static bool IsUnit(Unit sender)
+        {
+            return !(sender is Hero) && !(sender is Building) && 
+                ((sender.ClassID != ClassID.CDOTA_BaseNPC_Creep_Lane && sender.ClassID != ClassID.CDOTA_BaseNPC_Creep_Siege) || sender.IsControllable)
+                            && sender.ClassID != ClassID.CDOTA_NPC_TechiesMines
+                            && sender.ClassID != ClassID.CDOTA_NPC_Observer_Ward
+                            && sender.ClassID != ClassID.CDOTA_NPC_Observer_Ward_TrueSight;
+        }
+
+        private static void Entity_OnInt32PropertyChange(Entity sender, Int32PropertyChangeEventArgs args)
+        {
+            var unit = sender as Unit;
+            if (unit == null)
+                return;
+
+            if (args.PropertyName != "m_iTaggedAsVisibleByTeam")
+                return;
+
+            var player = ObjectManager.LocalPlayer;
+            if (player == null || player.Team == Team.Observer || sender.Team != player.Team)
+                return;
+
+            var visible = args.NewValue == 0x1E;
+            // heroes
+            if (sender is Hero && Menu.Item("heroes").GetValue<bool>())
+            {
+                HandleEffect(unit, visible);
+            }
+
+            // wards
+            else if (IsWard(sender) && Menu.Item("wards").GetValue<bool>())
+            {
+                HandleEffect(unit, visible);
+            }
+
+            // mines
+            else if (IsMine(sender) && Menu.Item("mines").GetValue<bool>())
+            {
+                HandleEffect(unit, visible);
+            }
+
+            // units
+            else if (Menu.Item("units").GetValue<bool>() && IsUnit(unit))
+            {
+                HandleEffect(unit, visible);
+            }
+
+            // buildings
+            else if (sender is Building && Menu.Item("buildings").GetValue<bool>())
+            {
+                HandleEffect(unit, visible);
+            }
+        }
+
+        private static void HandleEffect(Unit unit, bool visible)
         {
             if (!unit.IsValid)
             {
                 return;
             }
-            if (unit.IsVisibleToEnemies && unit.IsAlive)
+            if (visible && unit.IsAlive)
             {
                 ParticleEffect effect;
-                if (!Effects.TryGetValue(unit, out effect))
+                if (!_effects.TryGetValue(unit, out effect))
                 {
                     effect = unit.AddParticleEffect("particles/items_fx/aura_shivas.vpcf");
-                    Effects.Add(unit, effect);
+                    _effects.Add(unit, effect);
                 }
             }
             else
             {
                 ParticleEffect effect;
-                if (Effects.TryGetValue(unit, out effect))
+                if (_effects.TryGetValue(unit, out effect))
                 {
                     effect.Dispose();
-                    Effects.Remove(unit);
+                    _effects.Remove(unit);
                 }
-            }
-        }
-
-        private static void Game_OnIngameUpdate(EventArgs args)
-        {
-            var player = ObjectMgr.LocalPlayer;
-            if (player == null || player.Team == Team.Observer)
-            {
-                return;
-            }
-
-            // heroes
-            if (Menu.Item("heroes").GetValue<bool>())
-            {
-                HandleHeroes(player);
-            }
-
-            // wards
-            if (Menu.Item("wards").GetValue<bool>())
-            {
-                HandleWards(player);
-            }
-
-            // mines
-            if (Menu.Item("mines").GetValue<bool>())
-            {
-                HandleMines(player);
-            }
-
-            // units
-            if (Menu.Item("units").GetValue<bool>())
-            {
-                HandleUnits(player);
-            }
-
-            // buildings
-            if (Menu.Item("buildings").GetValue<bool>())
-            {
-                HandleBuildings(player);
-            }
-        }
-
-        private static void HandleBuildings(Entity player)
-        {
-            if (Utils.SleepCheck("VisibleByEnemy.UpdateBuildings"))
-            {
-                buildings = ObjectMgr.GetEntities<Building>().Where(x => x.Team == player.Team).ToList();
-                Utils.Sleep(1000, "VisibleByEnemy.UpdateBuildings");
-            }
-
-            foreach (var building in buildings)
-            {
-                HandleEffect(building);
-            }
-        }
-
-        private static void HandleHeroes(Entity player)
-        {
-            foreach (var hero in Heroes.GetByTeam(player.Team))
-            {
-                HandleEffect(hero);
-            }
-        }
-
-        private static void HandleMines(Entity player)
-        {
-            if (Utils.SleepCheck("VisibleByEnemy.UpdateMines"))
-            {
-                mines =
-                    ObjectMgr.GetEntities<Unit>()
-                        .Where(x => x.ClassID == ClassID.CDOTA_NPC_TechiesMines && x.Team == player.Team)
-                        .ToList();
-                Utils.Sleep(1000, "VisibleByEnemy.UpdateMines");
-            }
-
-            foreach (var mine in mines)
-            {
-                HandleEffect(mine);
-            }
-        }
-
-        private static void HandleUnits(Entity player)
-        {
-            if (Utils.SleepCheck("VisibleByEnemy.UpdateUnits"))
-            {
-                units =
-                    ObjectMgr.GetEntities<Unit>()
-                        .Where(
-                            x =>
-                            !(x is Hero) && !(x is Building) && x.ClassID != ClassID.CDOTA_BaseNPC_Creep_Lane
-                            && x.ClassID != ClassID.CDOTA_NPC_TechiesMines
-                            && x.ClassID != ClassID.CDOTA_NPC_Observer_Ward
-                            && x.ClassID != ClassID.CDOTA_NPC_Observer_Ward_TrueSight && x.Team == player.Team)
-                        .ToList();
-                Utils.Sleep(1000, "VisibleByEnemy.UpdateUnits");
-            }
-
-            foreach (var unit in units)
-            {
-                HandleEffect(unit);
-            }
-        }
-
-        private static void HandleWards(Entity player)
-        {
-            if (Utils.SleepCheck("VisibleByEnemy.UpdateWards"))
-            {
-                wards =
-                    ObjectMgr.GetEntities<Unit>()
-                        .Where(
-                            x =>
-                            (x.ClassID == ClassID.CDOTA_NPC_Observer_Ward
-                             || x.ClassID == ClassID.CDOTA_NPC_Observer_Ward_TrueSight) && x.Team == player.Team)
-                        .ToList();
-                Utils.Sleep(1000, "VisibleByEnemy.UpdateWards");
-            }
-
-            foreach (var ward in wards)
-            {
-                HandleEffect(ward);
             }
         }
 
         // ReSharper disable once InconsistentNaming
         private static void Item_ValueChanged(object sender, OnValueChangeEventArgs e)
         {
-            foreach (var particleEffect in Effects.Values)
-            {
-                particleEffect.Dispose();
-            }
+            var item = sender as MenuItem;
+            if (item == null)
+                return;
 
-            Effects.Clear();
+            bool hero = false, wards = false,  mines = false,units = false,buildings=false;
+            switch (item.Name)
+            {
+                case "heroes":
+                    hero = true;
+                    break;
+                case "wards":
+                    wards = true;
+                    break;
+                case "mines":
+                    mines = true;
+                    break;
+                case "units":
+                    units = true;
+                    break;
+                case "buildings":
+                    buildings = true;
+                    break;
+            }
+            // update dictionary
+            var newDict = new Dictionary<Unit,ParticleEffect>();
+            foreach (var effect in _effects)
+            {
+                if( hero && effect.Key is Hero )
+                    effect.Value.Dispose();
+                else if( wards && IsWard(effect.Key) )
+                    effect.Value.Dispose();
+                else if (mines && IsMine(effect.Key))
+                    effect.Value.Dispose();
+                else if (units && IsUnit(effect.Key))
+                    effect.Value.Dispose();
+                else if (buildings && effect.Key is Building)
+                    effect.Value.Dispose();
+                else
+                    newDict.Add(effect.Key,effect.Value);
+            }
+            _effects = newDict;
         }
 
         #endregion
