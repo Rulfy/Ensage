e08744a2e0deb1b7fcc9d783ab5f2f8939353fa6
Rulfy <git@zynox.net>
2016-12-02 13:59:53 +0100

    added illusion splitter

---
+++ IllusionSplitter/AssemblyMenu.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Ensage.Common.Menu;
+
+namespace IllusionSplitter
+{
+    public class AssemblyMenu
+    {
+        // main
+        private static Menu _menu;
+
+        // options
+        private static MenuItem _angleRandomizer;
+        private static MenuItem _moveHero;
+        private static MenuItem _minMoveRange;
+        private static MenuItem _useSpells;
+        private static MenuItem _useItems;
+        private static MenuItem _illuRange;
+
+        // hotkeys
+        private static MenuItem _splitterHotkeyItem;
+
+        // public getters
+        public static bool ShouldRandomizeAngle => _angleRandomizer.GetValue<bool>();
+        public static bool ShouldMoveHero => _moveHero.GetValue<bool>();
+        public static bool ShouldUseSpells => _useSpells.GetValue<bool>();
+        public static bool ShouldUseItems => _useItems.GetValue<bool>();
+        public static int IllusionRange => _illuRange.GetValue<Slider>().Value;
+        public static int MinimumMoveRange => _minMoveRange.GetValue<Slider>().Value;
+
+        public static event EventHandler SplitterHotkeyPressed;
+
+
+        public static bool BuildMenu()
+        {
+            if (_menu != null)
+                return false;
+
+            _menu = new Menu("Illusion Splitter", "illuSplitter", true);
+
+            // options
+            _angleRandomizer = new MenuItem("angleRandom", "Randomize Split Angle").SetValue(true);
+            _angleRandomizer.Tooltip = "Randomizes the split angle for illusions.";
+            _menu.AddItem(_angleRandomizer);
+
+            _moveHero = new MenuItem("moveHero", "Move Hero").SetValue(true);
+            _moveHero.Tooltip = "Moves your hero to your mouse position, after pressing the split hotkey.";
+            _menu.AddItem(_moveHero);
+
+            _minMoveRange = new MenuItem("minMoveRange", "Minimum Move Range").SetValue(new Slider(800, 100, 2000));
+            _minMoveRange.Tooltip = "Minimum range to move the illusions.";
+            _menu.AddItem(_minMoveRange);
+
+            _useSpells = new MenuItem("useSpells", "Use Spells").SetValue(true);
+            _useSpells.Tooltip = "Uses your spells to create illusions before splitting them.";
+            _menu.AddItem(_useSpells);
+
+            _useItems = new MenuItem("useItems", "Move Items").SetValue(true);
+            _useItems.Tooltip = "Uses your items to create illusions before splitting them.";
+            _menu.AddItem(_useItems);
+
+            _illuRange = new MenuItem("illuRange", "Illusion Range").SetValue(new Slider(600,100,2000));
+            _illuRange.Tooltip = "The range to find illusions near your hero.";
+            _menu.AddItem(_illuRange);
+
+            // hotkeys 
+            _splitterHotkeyItem = new MenuItem("splitKey", "Split Illusions").SetValue(new KeyBind(0, KeyBindType.Press));
+            _splitterHotkeyItem.Tooltip = "Splits all current illusions near your hero.";
+            _splitterHotkeyItem.ValueChanged += _splitterHotkeyItem_ValueChanged;
+            _menu.AddItem(_splitterHotkeyItem);
+
+
+            _menu.AddToMainMenu();
+            return true;
+        }
+
+        private static void _splitterHotkeyItem_ValueChanged(object sender, OnValueChangeEventArgs e)
+        {
+            if ((e.GetNewValue<KeyBind>().Key == e.GetOldValue<KeyBind>().Key) && e.GetNewValue<KeyBind>().Active)
+            {
+                SplitterHotkeyPressed?.Invoke(null, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ IllusionSplitter/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Ensage;
+using Ensage.Common;
+using Ensage.Common.Extensions;
+using Ensage.Common.Extensions.SharpDX;
+using Ensage.Common.Threading;
+using Ensage.Items;
+using log4net;
+using PlaySharp.Toolkit.Logging;
+using SharpDX;
+
+namespace IllusionSplitter
+{
+    class Program
+    {
+        private static readonly ILog Log = AssemblyLogs.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private static bool _added;
+
+        static void Main()
+        {
+            Events.OnLoad += Events_OnLoad;
+        }
+
+        private static void Events_OnLoad(object sender, EventArgs e)
+        {
+            if (!AssemblyMenu.BuildMenu())
+                return;
+
+            AssemblyMenu.SplitterHotkeyPressed += AssemblyMenu_SplitterHotkeyPressed;
+        }
+
+#pragma warning disable 1998
+        private static async void GameDispatcher_OnIngameUpdate(EventArgs args)
+#pragma warning restore 1998
+        {
+            Await.Block("spell", SplitterLogic);
+        }
+
+        private static void AssemblyMenu_SplitterHotkeyPressed(object sender, EventArgs e)
+        {
+            var hero = ObjectManager.LocalHero;
+            if (hero == null || _added)
+                return;
+            _added = true;
+            GameDispatcher.OnIngameUpdate += GameDispatcher_OnIngameUpdate;
+            
+        }
+
+        private static async Task UseSpells(Hero hero)
+        {
+            var mirrorImage = hero.FindSpell("naga_siren_mirror_image");
+            if (mirrorImage != null && mirrorImage.CanBeCasted())
+            {
+                mirrorImage.UseAbility();
+                int delay = (int) ((mirrorImage.GetCastPoint(0) +
+                                   mirrorImage.AbilitySpecialData.First(x => x.Name == "invuln_duration").Value)*1000.0f) +
+                            250;
+                Log.Debug($"using mirror image with delay {delay}");
+                await Task.Delay(delay);
+                return;
+            }
+
+            var conjureImage = hero.FindSpell("terrorblade_conjure_image");
+            if (conjureImage != null && conjureImage.CanBeCasted())
+            {
+                conjureImage.UseAbility();
+                int delay = (int)(conjureImage.GetCastPoint(0) * 1000.0f + 250.0f);
+                Log.Debug($"using conjure image with delay {delay}");
+                await Task.Delay(delay);
+                return;
+            }
+
+            var doppelWalk = hero.FindSpell("phantom_lancer_doppelwalk");
+            if (doppelWalk != null && doppelWalk.CanBeCasted())
+            {
+                var pos = Game.MousePosition - hero.Position;
+                if (pos.Length() > doppelWalk.CastRange)
+                {
+                    pos.Normalize();
+                    pos *= doppelWalk.CastRange;
+                }
+
+                doppelWalk.UseAbility(hero.Position + pos);
+                int delay = (int)(doppelWalk.GetCastPoint(0) +
+                                   doppelWalk.AbilitySpecialData.First(x => x.Name == "delay").Value) * 1000 +
+                            250;
+                Log.Debug($"using doppel walk with delay {delay}");
+                await Task.Delay(delay);
+                // ReSharper disable once RedundantJumpStatement
+                return;
+            }
+        }
+
+        private static async Task SplitterLogic()
+        {
+            var hero = ObjectManager.LocalHero;
+            bool casted = false;
+            // checks for items
+            if (AssemblyMenu.ShouldUseItems)
+            {
+                var manta = hero.FindItem("item_manta");
+                if (manta != null && manta.CanBeCasted())
+                {
+                    Log.Debug("Used manta");
+                    manta.UseAbility();
+                    casted = true;
+
+                    await Task.Delay(250);
+                }
+                if (!casted)
+                {
+                    var bottle = hero.FindItem("item_bottle") as Bottle;
+                    if (bottle != null && bottle.StoredRune == RuneType.Illusion)
+                    {
+                        Log.Debug("Used bottle");
+                        bottle.UseAbility();
+                        casted = true;
+
+                        await Task.Delay(125);
+                    }
+                }
+            }
+
+            if (!casted && AssemblyMenu.ShouldUseSpells)
+            {
+                await UseSpells(hero);
+            }
+
+            Vector3 heroTargetDirection;
+            if (AssemblyMenu.ShouldMoveHero)
+            {
+                Log.Debug($"Move hero to postition {Game.MousePosition}");
+                hero.Move(Game.MousePosition);
+                heroTargetDirection = Game.MousePosition - hero.Position;
+            }
+            else
+            {
+                heroTargetDirection = hero.InFront(250) - hero.Position;
+                Log.Debug($"Hero target dir {heroTargetDirection}");
+            }
+
+            var illusions =
+                ObjectManager.GetEntitiesFast<Hero>()
+                    .Where(x => x.IsIllusion && x.IsAlive && x.IsControllable && x.Distance2D(hero) < AssemblyMenu.IllusionRange)
+                    .ToList();
+            if (!illusions.Any())
+            {
+                GameDispatcher.OnIngameUpdate -= GameDispatcher_OnIngameUpdate;
+                _added = false;
+                return;
+            }
+
+            Vector3 middlePosition = illusions.Aggregate(hero.Position, (current, illusion) => current + illusion.Position);
+            var unitCount = illusions.Count + 1;
+
+            middlePosition /= unitCount;
+            float illuAngle = 360.0f / unitCount;
+
+            Random random = null;
+            if (AssemblyMenu.ShouldRandomizeAngle)
+                random = new Random();
+
+            foreach (var illusion in illusions)
+            {
+                if (random != null)
+                {
+                    var randomAngle = random.NextFloat(1, illuAngle / unitCount);
+                    heroTargetDirection = heroTargetDirection.Rotated(MathUtil.DegreesToRadians(illuAngle + randomAngle));
+                }
+                else
+                    heroTargetDirection = heroTargetDirection.Rotated(MathUtil.DegreesToRadians(illuAngle));
+
+                var dir = heroTargetDirection.Normalized();
+                dir *= AssemblyMenu.MinimumMoveRange;
+                var movePos = middlePosition + dir;
+                Log.Debug($"Move illu to {movePos}");
+                illusion.Move(movePos);
+            }
+            GameDispatcher.OnIngameUpdate -= GameDispatcher_OnIngameUpdate;
+            _added = false;
+        }
+    }
+}
