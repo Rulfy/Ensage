426a1b1474e10149383f0753fb68db424ab8ccd1
Rulfy <git@zynox.net>
2017-02-08 18:05:17 +0100

    added multiitemuser

---
+++ MultiItemUser/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using Ensage;
+using Ensage.Common;
+using Ensage.Common.Extensions;
+
+namespace MultiItemUser
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Player.OnExecuteOrder += Player_OnExecuteOrder;
+            Events.OnLoad += Events_OnLoad;
+        }
+
+        private static void Events_OnLoad(object sender, EventArgs e)
+        {
+            Game.PrintMessage("MultiItemUser loaded!");
+        }
+
+        private static void Player_OnExecuteOrder(Player sender, ExecuteOrderEventArgs args)
+        {
+            if (args.Order == Order.Ability)
+            {
+                var owner = args.Ability.Owner as Unit;
+                if (owner != null)
+                {
+                    var id = args.Ability.GetAbilityId();
+                    var abilities = owner.Inventory.Items.Where(x => x.GetAbilityId() == id).ToList();
+                    if (abilities.Count() > 1)
+                    {
+                        foreach (var ability in abilities)
+                        {
+                            ability.UseAbility();
+                        }
+                        args.Process = false;
+                    }
+                }
+            }
+            else if (args.Order == Order.AbilityTarget)
+            {
+                var owner = args.Ability.Owner as Unit;
+                if (owner != null)
+                {
+                    var id = args.Ability.GetAbilityId();
+                    var abilities = owner.Inventory.Items.Where(x => x.GetAbilityId() == id).ToList();
+                    if (abilities.Count() > 1)
+                    {
+                        var target = args.Target as Unit;
+                        foreach (var ability in abilities)
+                        {
+                            ability.UseAbility(target);
+                        }
+                        args.Process = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MultiItemUser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MultiItemUser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MultiItemUser")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f9c1d83-f8b2-45d6-98bb-5ff200a57f27")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
