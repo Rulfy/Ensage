d2098820aa5598aa1331a97e375ae13cbe723f2e
Rulfy <git@zynox.net>
2017-03-13 17:14:38 +0100

    fixed doing stuff in the Main function

--- VisibleByEnemy/Program.cs
+++ VisibleByEnemy/Program.cs
@@ -1,12 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using Ensage;
+using Ensage.Common;
+using Ensage.Common.Menu;
 
 namespace VisibleByEnemy
 {
-    using System.Collections.Generic;
-    using Ensage;
-    using Ensage.Common.Menu;
-
     internal class Program
     {
         #region Static Fields
@@ -15,45 +15,57 @@ namespace VisibleByEnemy
 
         private static readonly Menu Menu = new Menu("VisibleByEnemy", "visibleByEnemy", true);
 
+        private static bool _loaded;
+
         #endregion
 
         #region Public Methods and Operators
 
         private static void Main()
         {
-            var item = new MenuItem("heroes", "Check allied heroes").SetValue(true);
-            item.ValueChanged += Item_ValueChanged;
-            Menu.AddItem(item);
-
-            item = new MenuItem("wards", "Check wards").SetValue(true);
-            item.ValueChanged += Item_ValueChanged;
-            Menu.AddItem(item);
-
-            item = new MenuItem("mines", "Check techies mines").SetValue(true);
-            item.ValueChanged += Item_ValueChanged;
-            Menu.AddItem(item);
-
-            item = new MenuItem("units", "Check controlled units (not lane creeps)").SetValue(true);
-            item.ValueChanged += Item_ValueChanged;
-            Menu.AddItem(item);
-
-            item = new MenuItem("buildings", "Check buildings").SetValue(true);
-            item.ValueChanged += Item_ValueChanged;
-            Menu.AddItem(item);
-
-            Menu.AddToMainMenu();
-
-            LoopEntities();
+            Events.OnLoad += Events_OnLoad;
             Entity.OnInt32PropertyChange += Entity_OnInt32PropertyChange;
+        }
+
+        private static void Events_OnLoad(object sender, EventArgs e)
+        {
+            if (!_loaded)
+            {
+                _loaded = true;
+
+                var item = new MenuItem("heroes", "Check allied heroes").SetValue(true);
+                item.ValueChanged += Item_ValueChanged;
+                Menu.AddItem(item);
+
+                item = new MenuItem("wards", "Check wards").SetValue(true);
+                item.ValueChanged += Item_ValueChanged;
+                Menu.AddItem(item);
+
+                item = new MenuItem("mines", "Check techies mines").SetValue(true);
+                item.ValueChanged += Item_ValueChanged;
+                Menu.AddItem(item);
+
+                item = new MenuItem("units", "Check controlled units (not lane creeps)").SetValue(true);
+                item.ValueChanged += Item_ValueChanged;
+                Menu.AddItem(item);
+
+                item = new MenuItem("buildings", "Check buildings").SetValue(true);
+                item.ValueChanged += Item_ValueChanged;
+                Menu.AddItem(item);
+
+                Menu.AddToMainMenu();
+            }
+            LoopEntities();
         }
 
         #endregion
 
         #region Methods
+
         private static bool IsWard(Entity sender)
         {
-            return (sender.ClassID == ClassID.CDOTA_NPC_Observer_Ward ||
-                    sender.ClassID == ClassID.CDOTA_NPC_Observer_Ward_TrueSight);
+            return sender.ClassID == ClassID.CDOTA_NPC_Observer_Ward ||
+                   sender.ClassID == ClassID.CDOTA_NPC_Observer_Ward_TrueSight;
         }
 
         private static bool IsMine(Entity sender)
@@ -63,25 +75,32 @@ namespace VisibleByEnemy
 
         private static bool IsUnit(Unit sender)
         {
-            return !(sender is Hero) && !(sender is Building) && 
-                ((sender.ClassID != ClassID.CDOTA_BaseNPC_Creep_Lane && sender.ClassID != ClassID.CDOTA_BaseNPC_Creep_Siege) || sender.IsControllable)
-                            && sender.ClassID != ClassID.CDOTA_NPC_TechiesMines
-                            && sender.ClassID != ClassID.CDOTA_NPC_Observer_Ward
-                            && sender.ClassID != ClassID.CDOTA_NPC_Observer_Ward_TrueSight;
+            return !(sender is Hero) && !(sender is Building) &&
+                   (sender.ClassID != ClassID.CDOTA_BaseNPC_Creep_Lane &&
+                    sender.ClassID != ClassID.CDOTA_BaseNPC_Creep_Siege || sender.IsControllable)
+                   && sender.ClassID != ClassID.CDOTA_NPC_TechiesMines
+                   && sender.ClassID != ClassID.CDOTA_NPC_Observer_Ward
+                   && sender.ClassID != ClassID.CDOTA_NPC_Observer_Ward_TrueSight;
         }
 
         private static void Entity_OnInt32PropertyChange(Entity sender, Int32PropertyChangeEventArgs args)
         {
             var unit = sender as Unit;
             if (unit == null)
+            {
                 return;
+            }
 
             if (args.PropertyName != "m_iTaggedAsVisibleByTeam")
+            {
                 return;
+            }
 
             var player = ObjectManager.LocalPlayer;
             if (player == null || player.Team == Team.Observer || sender.Team != player.Team)
+            {
                 return;
+            }
 
             var visible = args.NewValue == 0x1E;
             // heroes
@@ -118,8 +137,10 @@ namespace VisibleByEnemy
         private static void LoopEntities()
         {
             var player = ObjectManager.LocalPlayer;
-            if (player == null || player.Team == Team.Observer )
+            if (player == null || player.Team == Team.Observer)
+            {
                 return;
+            }
             var units = ObjectManager.GetEntities<Unit>().Where(x => x.Team == player.Team).ToList();
             if (Menu.Item("heroes").GetValue<bool>())
             {
@@ -128,7 +149,7 @@ namespace VisibleByEnemy
                     HandleEffect(hero, hero.IsVisibleToEnemies);
                 }
             }
-            if ( Menu.Item("wards").GetValue<bool>())
+            if (Menu.Item("wards").GetValue<bool>())
             {
                 foreach (var ward in units.Where(IsWard).ToList())
                 {
@@ -189,9 +210,11 @@ namespace VisibleByEnemy
         {
             var item = sender as MenuItem;
             if (item == null)
+            {
                 return;
+            }
 
-            bool hero = false, wards = false,  mines = false,units = false,buildings=false;
+            bool hero = false, wards = false, mines = false, units = false, buildings = false;
             switch (item.Name)
             {
                 case "heroes":
@@ -211,21 +234,33 @@ namespace VisibleByEnemy
                     break;
             }
             // update dictionary
-            var newDict = new Dictionary<Unit,ParticleEffect>();
+            var newDict = new Dictionary<Unit, ParticleEffect>();
             foreach (var effect in _effects)
             {
-                if( hero && effect.Key is Hero )
+                if (hero && effect.Key is Hero)
+                {
                     effect.Value.Dispose();
-                else if( wards && IsWard(effect.Key) )
+                }
+                else if (wards && IsWard(effect.Key))
+                {
                     effect.Value.Dispose();
+                }
                 else if (mines && IsMine(effect.Key))
+                {
                     effect.Value.Dispose();
+                }
                 else if (units && IsUnit(effect.Key))
+                {
                     effect.Value.Dispose();
+                }
                 else if (buildings && effect.Key is Building)
+                {
                     effect.Value.Dispose();
+                }
                 else
-                    newDict.Add(effect.Key,effect.Value);
+                {
+                    newDict.Add(effect.Key, effect.Value);
+                }
             }
             _effects = newDict;
         }
