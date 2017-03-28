d0505fee488df1630d649356a5e351665520a738
Rulfy <git@zynox.net>
2017-03-29 00:23:38 +0200

    added antimage

--- Zaio/Heroes/AntiMage.cs
+++ Zaio/Heroes/AntiMage.cs
@@ -1,52 +1,177 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading;
-using System.Threading.Tasks;
-
-using Ensage;
-using Ensage.Common.Extensions;
-using Ensage.Common.Extensions.SharpDX;
-using Ensage.Common.Menu;
-using Ensage.Common.Threading;
-
-using log4net;
-
-using PlaySharp.Toolkit.Logging;
-
-using Zaio.Helpers;
-using Zaio.Interfaces;
-using Zaio.Prediction;
-
-using AbilityId = Ensage.Common.Enums.AbilityId;
-using Prediction = Ensage.Common.Prediction;
-
-
 namespace Zaio.Heroes
 {
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Ensage;
+    using Ensage.Common;
+    using Ensage.Common.Enums;
+    using Ensage.Common.Extensions;
+    using Ensage.Common.Extensions.SharpDX;
+    using Ensage.Common.Menu;
+    using Ensage.Common.Threading;
+
+    using log4net;
+
+    using PlaySharp.Toolkit.Logging;
+
+    using SharpDX;
+
+    using Zaio.Helpers;
+    using Zaio.Interfaces;
+
+    using AbilityId = Ensage.Common.Enums.AbilityId;
+
     [Hero(ClassID.CDOTA_Unit_Hero_AntiMage)]
     internal class AntiMage : ComboHero
     {
+        private static readonly string[] KillstealAbilities = { "antimage_mana_void" };
+
         private static readonly ILog Log = AssemblyLogs.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-        private static readonly string[] SupportedAbilities =
-        {
-            "antimage_blink",
-            "antimage_mana_void"
-        };
-
-        private static readonly string[] KillstealAbilities =
-        {
-            "antimage_mana_void"
-        };
+        private static readonly string[] SupportedAbilities = { "antimage_blink", "antimage_mana_void", "item_manta" };
 
         private Ability _blinkAbility;
 
-        private Ability _ultAbility;
-
         private MenuItem _minimumEnemyUltCount;
 
+        private Ability _ultAbility;
+
         private int EnemyCountForUlt => this._minimumEnemyUltCount.GetValue<Slider>().Value;
+
+        public override async Task ExecuteComboAsync(Unit target, CancellationToken tk = new CancellationToken())
+        {
+            var manta = this.MyHero.GetItemById(ItemId.item_manta);
+
+            if (!await this.MoveOrBlinkToEnemy(target, tk, 0, 150))
+            {
+                if (!this.MyHero.IsSilenced())
+                {
+                    if (this._blinkAbility.IsAbilityEnabled() && this._blinkAbility.CanBeCasted(target)
+                        && this._blinkAbility.CanHit(target))
+                    {
+                        var usedManta = false;
+                        if (manta != null && manta.IsAbilityEnabled() && manta.CanBeCasted() && this.MyHero.IsSilenced())
+                        {
+                            Log.Debug($"use manta 1 because silenced");
+                            manta.UseAbility();
+                            await Await.Delay(125, tk);
+                            manta = null;
+                            usedManta = true;
+                        }
+
+                        if (!usedManta)
+                        {
+                            if (this.MyHero.Distance2D(target) >= 350)
+                            {
+                                Vector3 pos;
+                                if (!target.IsMoving)
+                                {
+                                    pos = (target.NetworkPosition - this.MyHero.NetworkPosition).Normalized();
+                                    pos *= 100;
+                                    pos = target.NetworkPosition - pos;
+                                }
+                                else
+                                {
+                                    pos = Prediction.InFront(target, 250);
+                                }
+
+                                Log.Debug($"jumping {pos}");
+                                this._blinkAbility.UseAbility(pos);
+                                await Await.Delay(this.GetAbilityDelay(pos, this._blinkAbility), tk);
+
+                                if (manta != null && manta.IsAbilityEnabled() && manta.CanBeCasted())
+                                {
+                                    Log.Debug($"use manta after blink");
+                                    manta.UseAbility();
+                                    await Await.Delay(125, tk);
+                                }
+                            }
+                        }
+                            
+                    }
+                }
+            }
+
+            await this.HasNoLinkens(target, tk);
+            await this.UseItems(target, tk);
+            await this.DisableEnemy(target, tk);
+
+            if (!this.MyHero.IsSilenced())
+            {
+                if (this._ultAbility.IsAbilityEnabled() && this._ultAbility.CanBeCasted())
+                {
+                    var radius = this._ultAbility.GetAbilityData("mana_void_aoe_radius");
+                    var damage = this._ultAbility.GetAbilityData("mana_void_damage_per_mana");
+                    var spellAmp = this.GetSpellAmp();
+
+                    var targetHealth = target.Health * (1.0f + target.MagicResistance());
+
+                    var enemy =
+                        ObjectManager.GetEntitiesParallel<Hero>()
+                                     .Where(
+                                         x =>
+                                             x.IsValid && x.IsAlive && x.IsVisible && x.Team != this.MyHero.Team
+                                             && this._ultAbility.CanHit(x)
+                                             && targetHealth < (x.MaximumMana - x.Mana) * damage * spellAmp
+                                             && x.Distance2D(target) < radius)
+                                     .OrderByDescending(x => x.MaximumMana - x.Mana)
+                                     .FirstOrDefault();
+                    if (enemy != null)
+                    {
+                        var useUlt = this.EnemyCountForUlt == 0;
+                        if (!useUlt)
+                        {
+                            var enemyDamage = (enemy.MaximumMana - enemy.Mana) * damage * spellAmp;
+                            var enemyCount =
+                                ObjectManager.GetEntitiesParallel<Hero>()
+                                             .Count(
+                                                 x =>
+                                                     x.IsValid && x.IsAlive && x.IsVisible
+                                                     && x.Team != this.MyHero.Team && !x.IsIllusion
+                                                     && x.Health < enemyDamage * (1.0f - x.MagicResistance())
+                                                     && x.Distance2D(enemy) < radius);
+                            useUlt = this.EnemyCountForUlt <= enemyCount;
+                        }
+
+                        if (useUlt)
+                        {
+                            Log.Debug(
+                                $"Kill it with fire {targetHealth} < {(enemy.MaximumMana - enemy.Mana) * damage * spellAmp}");
+                            this._ultAbility.UseAbility(enemy);
+                            await Await.Delay(this.GetAbilityDelay(enemy, this._ultAbility), tk);
+                        }
+                    }
+                }
+            }
+
+            if (ZaioMenu.ShouldUseOrbwalker)
+            {
+                this.Orbwalk();
+            }
+            else
+            {
+                this.MyHero.Attack(target);
+                await Await.Delay(125, tk);
+            }
+
+            var illusions =
+                ObjectManager.GetEntitiesParallel<Hero>()
+                             .Where(
+                                 x =>
+                                     x.IsValid && x.IsAlive && x.IsIllusion && x.IsControllable && x.Team == this.MyHero.Team
+                                     && x.Distance2D(this.MyHero) < 1000);
+            if (illusions.Any())
+            {
+                foreach (var illusion in illusions)
+                {
+                    illusion.Attack(target);
+                }
+                await Await.Delay(125, tk);
+            }
+        }
 
         public override void OnLoad()
         {
@@ -65,16 +190,16 @@ namespace Zaio.Heroes
             heroMenu.AddItem(supportedKillsteal);
 
             this._minimumEnemyUltCount =
-            new MenuItem("zaioAntiMageMinEnemyCount", "Minimum Enemies for Ult").SetValue(new Slider(1, 0, 4));
-            this._minimumEnemyUltCount.Tooltip = "Minimum enemies besides your target to use ult. Killsteal must be enabled!";
+                new MenuItem("zaioAntiMageMinEnemyCount", "Minimum Enemies for Ult").SetValue(new Slider(1, 0, 4));
+            this._minimumEnemyUltCount.Tooltip = "Minimum enemies besides your target to use ult. Also used for killsteal with (+1)!";
             heroMenu.AddItem(this._minimumEnemyUltCount);
 
             this.OnLoadMenuItems(supportedStuff, supportedKillsteal);
 
             ZaioMenu.LoadHeroSettings(heroMenu);
 
-            this._blinkAbility = MyHero.GetAbilityById(AbilityId.antimage_blink);
-            this._ultAbility = MyHero.GetAbilityById(AbilityId.antimage_mana_void);
+            this._blinkAbility = this.MyHero.GetAbilityById(AbilityId.antimage_blink);
+            this._ultAbility = this.MyHero.GetAbilityById(AbilityId.antimage_mana_void);
         }
 
         protected override async Task<bool> Killsteal()
@@ -84,170 +209,58 @@ namespace Zaio.Heroes
                 return true;
             }
 
-            if (!this.MyHero.IsSilenced() && this._ultAbility.IsKillstealAbilityEnabled() && this._ultAbility.CanBeCasted())
+            if (this.MyHero.IsSilenced())
             {
-                var damage = _ultAbility.GetAbilityData("mana_void_damage_per_mana");
-                //Create a list of all enemies which can be ulted by AM
-                List<Hero> enemies = null;
-                enemies =
+                return false;
+            }
+
+            if (this._ultAbility.IsKillstealAbilityEnabled() && this._ultAbility.CanBeCasted())
+            {
+                var enemies =
                     ObjectManager.GetEntitiesParallel<Hero>()
-                        .Where(
-                             x =>
-                                 x.IsAlive && x.Team != this.MyHero.Team && !x.IsIllusion
-                                 && this._ultAbility.CanBeCasted() && this._ultAbility.CanHit(x)
-                                 && !x.IsLinkensProtected()
-                                 && !x.CantBeAttacked() && !x.CantBeKilled())
-                                 .ToList();
-
-                var current_damage = 0.0;
-                var previous_target = default(Ensage.Hero);
-                var best_target = default(Ensage.Hero);
-                var killable_enemies = 0;
-
-                //Radius KS
-                foreach (var enemy in enemies)
+                                 .Where(
+                                     x =>
+                                         x.IsValid && x.IsAlive && x.IsVisible && x.Team != this.MyHero.Team
+                                         && !x.IsIllusion && this._ultAbility.CanHit(x))
+                                 .OrderByDescending(x => x.MaximumMana - x.Mana);
+                if (enemies.Any())
                 {
+                    var damage = this._ultAbility.GetAbilityData("mana_void_damage_per_mana");
+                    var radius = this._ultAbility.GetAbilityData("mana_void_aoe_radius");
+                    var spellAmp = this.GetSpellAmp();
 
-                    var possible_damage = damage;
-                    possible_damage *= (enemy.MaximumMana - enemy.Mana);
-
-                    //Check if the possible damage exceeds what the damage would be to our previous enemy
-                    if (possible_damage >= current_damage)
+                    var targetCount = 0;
+                    Hero bestTarget = null;
+                    foreach (var enemy in enemies)
                     {
-                        best_target = enemy;
-                    }
-
-                    //Set the current damage to this enemy
-                    current_damage = possible_damage;
-
-                    //Check if an enemy is killable in the radius of AM's ult
-                    var radius = _ultAbility.GetAbilityData("mana_void_aoe_radius");
-                    var polygon = new Geometry.Polygon.Circle(best_target.NetworkPosition, radius);
-
-                    if(previous_target != default(Ensage.Hero) && polygon.IsInside(previous_target.NetworkPosition) 
-                      && (current_damage * (1 - previous_target.MagicResistance()) >= previous_target.Health))
-                    {
-                        //Add enemy to amount that can be killed
-                        killable_enemies += 1;
-
-                        //Ult a low target mana if it can kill one of their teammates
-                        if (EnemyCountForUlt == 0 && killable_enemies >= EnemyCountForUlt)
+                        var enemyDamage = (enemy.MaximumMana - enemy.Mana) * damage * spellAmp;
+                        var currentCount =
+                            ObjectManager.GetEntitiesParallel<Hero>()
+                                         .Count(
+                                             x =>
+                                                 x.IsValid && x.IsAlive && x.IsVisible && x.Team != this.MyHero.Team
+                                                 && !x.IsIllusion && !x.CantBeAttacked() && !x.CantBeKilled()
+                                                 && (x == enemy || x.Distance2D(enemy) < radius)
+                                                 && x.Health <= enemyDamage * (1.0f - x.MagicResistance()));
+                        if (targetCount < currentCount)
                         {
-                            this._ultAbility.UseAbility(best_target);
-                            await Await.Delay(this.GetAbilityDelay(this._ultAbility));
+                            targetCount = currentCount;
+                            bestTarget = enemy;
                         }
-
-
                     }
 
-                    //Regular KS based on amount of targets that can be killed
-                    var enemy_r =
-                        ObjectManager.GetEntitiesParallel<Hero>()
-                            .FirstOrDefault(
-                                x =>
-                                    x.IsAlive && x.Team != this.MyHero.Team && !x.IsIllusion
-                                    && this._ultAbility.CanBeCasted() && this._ultAbility.CanHit(x)
-                                    && x.Health < (damage * (x.MaximumMana - x.Mana) * (1 - x.MagicResistance()))
-                                    && !x.IsLinkensProtected() && !x.CantBeAttacked() && !x.CantBeKilled());
-
-                    if (enemy_r != null && killable_enemies >= EnemyCountForUlt 
-                        && (best_target.Health < ((damage * (best_target.MaximumMana - best_target.Mana)) * (1 - best_target.MagicResistance()))))
+                    Log.Debug($"killsteal count {targetCount}");
+                    if (bestTarget != null && targetCount >= this.EnemyCountForUlt + 1)
                     {
-                        Log.Debug($"{killable_enemies} {EnemyCountForUlt}");
-                        this._ultAbility.UseAbility(best_target);
-                        await Await.Delay(this.GetAbilityDelay(this._ultAbility));
+                        Log.Debug($"ks ulti on {bestTarget.Name} with {targetCount} enemies killing");
+                        this._ultAbility.UseAbility(bestTarget);
+                        await Await.Delay(this.GetAbilityDelay(bestTarget, this._ultAbility));
+                        return true;
                     }
-
-                    previous_target = enemy;
-
                 }
-
-
-
             }
 
             return false;
         }
-
-        public override async Task ExecuteComboAsync(Unit target, CancellationToken tk = new CancellationToken())
-        {
-
-          if (!await this.MoveOrBlinkToEnemy(target, tk, 0, 150))
-          {
-
-            if (!this.MyHero.IsSilenced())
-            {
-                if (this._blinkAbility.CanHit(target)
-                && this._blinkAbility.IsAbilityEnabled()
-                && this._blinkAbility.CanBeCasted(target))
-                {
-                        bool usePrediction = false;
-
-                        if (target.IsMoving)
-                        {
-                            usePrediction = true;
-                        }
-                        if (MyHero.Distance2D(target) >= 350)
-                        {
-                            if (!usePrediction)
-                            {
-                                var pos = (target.NetworkPosition - MyHero.NetworkPosition).Normalized();
-                                pos *= 150;
-                                pos = target.NetworkPosition - pos;
-                                Log.Debug($"Jumping");
-                                this._blinkAbility.UseAbility(pos);
-                            }
-
-                            if (usePrediction)
-                            {
-                                Log.Debug($"jumping");
-                                var jump_pos = Ensage.Common.Prediction.InFront(target, 250);
-                                this._blinkAbility.UseAbility(jump_pos);
-                            }
-                        }
-
-                    
-                  }
-
-                
-            }
-          }
-
-            await HasNoLinkens(target, tk);
-            await UseItems(target, tk);
-            await DisableEnemy(target, tk);
-
-            if(!MyHero.IsSilenced())
-            {
-                if (_ultAbility.IsAbilityEnabled() && _ultAbility.CanBeCasted(target) && _ultAbility.CanHit(target) && EnemyCountForUlt == 0)
-                {
-                    var damage = _ultAbility.GetAbilityData("mana_void_damage_per_mana");
-                    var target_mana = target.Mana;
-                    var total_mana = target.MaximumMana;
-                    damage *= total_mana - target_mana;
-                    damage = damage * (1 - target.MagicResistance());
-
-
-                    if (damage > target.Health)
-                    {
-                        Log.Debug($"Kill it with fire");
-                        this._ultAbility.UseAbility(target);
-                    }
-                }
-            }
-
-            if (ZaioMenu.ShouldUseOrbwalker)
-            {
-                Orbwalk();
-            }
-
-            else
-            {
-                MyHero.Attack(target);
-                await Await.Delay(125, tk);
-            }
-        }
-
-
     }
 }
