ba1ee28efb83e54f7bd135bf8df432aa0f30fc83
Rulfy <git@zynox.net>
2017-05-19 05:37:52 +0200

    minor fixes+greed mode started

--- Snatcher/Program.cs
+++ Snatcher/Program.cs
@@ -4,25 +4,25 @@
 
 namespace Snatcher
 {
+    using System;
     using System.ComponentModel.Composition;
     using System.Linq;
+    using System.Threading.Tasks;
 
     using Ensage;
-    using Ensage.Common.Menu;
     using Ensage.SDK.Extensions;
-    using Ensage.SDK.Handlers;
     using Ensage.SDK.Helpers;
     using Ensage.SDK.Service;
     using Ensage.SDK.Service.Metadata;
 
-    [ExportPlugin("Snatcher", StartupMode.Auto)]
+    [ExportPlugin("Snatcher")]
     public class Program : Plugin
     {
+        private const float PickUpDistance = 150.0f;
+
         private readonly Unit owner;
 
         private SnatcherConfig config;
-
-        private IUpdateHandler updateHandler;
 
         [ImportingConstructor]
         public Program([Import] IServiceContext context)
@@ -33,79 +33,130 @@ namespace Snatcher
         protected override void OnActivate()
         {
             this.config = new SnatcherConfig();
-            this.config.ScanIntervall.Item.ValueChanged += this.ItemValueChanged;
-            this.updateHandler = UpdateManager.Subscribe(this.OnUpdate, this.config.ScanIntervall);
+            UpdateManager.BeginInvoke(this.OnUpdate);
         }
 
         protected override void OnDeactivate()
         {
-            UpdateManager.Unsubscribe(this.OnUpdate);
-            this.config.ScanIntervall.Item.ValueChanged -= this.ItemValueChanged;
             this.config?.Dispose();
         }
 
-        private void ItemValueChanged(object sender, OnValueChangeEventArgs e)
+        private async Task ApproachEntity(Ability blinkAbility, Entity entity, float checkRange)
         {
-            var handler = this.updateHandler?.Executor as TimeoutHandler;
-            if (handler != null)
+            if (blinkAbility != null)
             {
-                handler.Timeout = e.GetNewValue<Slider>().Value;
+                var distance = this.owner.Distance2D(entity);
+                if (distance >= PickUpDistance)
+                {
+                    var dir = (entity.Position - this.owner.Position).Normalized();
+                    dir *= distance >= checkRange ? checkRange : distance;
+                    blinkAbility.UseAbility(this.owner.Position + dir);
+                    await Task.Delay(125);
+                }
             }
         }
 
-        private void OnUpdate()
+        private async void OnUpdate()
         {
-            if (!this.config.HoldHotkey.Value.Active && !this.config.ToggleHotkey.Value.Active)
+            while (this.IsActive)
             {
-                return;
-            }
+                await Task.Delay(this.config.ScanIntervall);
 
-            var hasFreeSlots = this.owner.Inventory.FreeInventorySlots.Any();
-            var freeBackbackSlots = this.owner.Inventory.FreeBackpackSlots.ToList();
-
-            bool swapItem = this.config.SwapItem;
-            if (hasFreeSlots || (swapItem && freeBackbackSlots.Any()))
-            {
-                var grabAegis = this.config.SnatchOptions.Value.IsEnabled("item_aegis");
-                var grabRapier = this.config.SnatchOptions.Value.IsEnabled("item_rapier");
-                var grabCheese = this.config.SnatchOptions.Value.IsEnabled("item_cheese");
-                var grabGem = this.config.SnatchOptions.Value.IsEnabled("item_gem");
-
-                if (grabAegis || grabRapier || grabCheese || grabGem)
+                if (!this.config.HoldHotkey.Value.Active && !this.config.ToggleHotkey.Value.Active)
                 {
-                    var query = EntityManager<PhysicalItem>.Entities.Where(x => x.IsVisible && x.Distance2D(this.owner) < 400);
-                    query = query.Where(
-                        x => (grabAegis && x.Item.Id == AbilityId.item_aegis) || (grabCheese && x.Item.Id == AbilityId.item_cheese)
-                             || (grabRapier && x.Item.Id == AbilityId.item_rapier) || (grabGem && x.Item.Id == AbilityId.item_gem));
+                    continue;
+                }
 
-                    var physicalItem = query.FirstOrDefault();
-                    if (physicalItem != null)
+                if (Game.IsPaused || !this.owner.IsAlive)
+                {
+                    continue;
+                }
+
+                Ability blinkAbility = null;
+                float checkRange = 300;
+
+                // if (this.config.GreedMode.Value.Dictionary.Any(x => x.Value))
+                if (this.config.GreedMode)
+                {
+                    var blinkItem = this.owner.GetItemById(AbilityId.item_blink);
+                    if (blinkItem?.Cooldown <= 0)
                     {
-                        if (!hasFreeSlots)
+                        checkRange = blinkItem.GetAbilitySpecialData("blink_range");
+                        blinkAbility = blinkItem;
+                    }
+
+                    // else
+                    // {
+                    // var blink = this.owner.GetAbilityById(AbilityId.antimage_blink);
+                    // blink = this.owner.GetAbilityById(AbilityId.queenofpain_blink);
+                    // }
+                }
+
+                var hasFreeSlots = this.owner.Inventory.FreeInventorySlots.Any();
+                var freeBackbackSlots = this.owner.Inventory.FreeBackpackSlots.ToList();
+
+                bool swapItem = this.config.SwapItem;
+                if (hasFreeSlots || (swapItem && freeBackbackSlots.Any()))
+                {
+                    var grabAegis = this.config.SnatchOptions.Value.IsEnabled("item_aegis");
+                    var grabRapier = this.config.SnatchOptions.Value.IsEnabled("item_rapier");
+                    var grabCheese = this.config.SnatchOptions.Value.IsEnabled("item_cheese");
+                    var grabGem = this.config.SnatchOptions.Value.IsEnabled("item_gem");
+
+                    if (grabAegis || grabRapier || grabCheese || grabGem)
+                    {
+                        var query = EntityManager<PhysicalItem>.Entities.Where(x => x.IsVisible && x.Distance2D(this.owner) < (checkRange + PickUpDistance));
+                        query = query.Where(
+                            x => (grabAegis && x.Item.Id == AbilityId.item_aegis) || (grabCheese && x.Item.Id == AbilityId.item_cheese)
+                                 || (grabRapier && x.Item.Id == AbilityId.item_rapier) || (grabGem && x.Item.Id == AbilityId.item_gem));
+
+                        var physicalItem = query.FirstOrDefault();
+                        if (physicalItem != null)
                         {
-                            // swap lowest cost item
-                            var item = this.owner.Inventory.Items.Where(x => x.IsKillable && x.Id != AbilityId.item_aegis).OrderBy(x => x.Cost).FirstOrDefault();
-                            if (item == null)
+                            if (!hasFreeSlots)
                             {
-                                return;
+                                // check if hero already has a cheese
+                                if (physicalItem.Item.Id != AbilityId.item_cheese || this.owner.GetItemById(AbilityId.item_cheese) != null)
+                                {
+                                    // swap lowest cost item
+                                    var item = this.owner.Inventory.Items.Where(x => x.IsKillable && x.Id != AbilityId.item_aegis).OrderBy(x => x.Cost).FirstOrDefault();
+                                    if (item == null)
+                                    {
+                                        continue;
+                                    }
+
+                                    item.MoveItem(freeBackbackSlots.First());
+                                }
                             }
 
-                            item.MoveItem(freeBackbackSlots.First());
+                            // if (this.config.GreedMode.Value.IsEnabled(physicalItem.Item.Name))
+                            if (blinkAbility != null)
+                            {
+                                await this.ApproachEntity(blinkAbility, physicalItem, checkRange);
+                            }
+                            
+                            this.owner.PickUpItem(physicalItem);
+                            continue;
                         }
-
-                        this.owner.PickUpItem(physicalItem);
-                        return;
                     }
                 }
-            }
 
-            if (this.config.SnatchOptions.Value.IsEnabled("rune_bounty"))
-            {
-                var runes = EntityManager<Rune>.Entities.Where(x => x.IsVisible && x.Distance2D(this.owner) < 400).ToList();
-                if (runes.Any())
+                if (this.config.SnatchOptions.Value.IsEnabled("rune_bounty"))
                 {
-                    this.owner.PickUpRune(runes.First());
-                    return;
+                    var runes = EntityManager<Rune>.Entities.Where(x => x.IsVisible && x.Distance2D(this.owner) < (checkRange + PickUpDistance)).ToList();
+                    if (runes.Any())
+                    {
+                        var rune = runes.First();
+
+                        // if (this.config.GreedMode.Value.IsEnabled("rune_bounty"))
+                        if (blinkAbility != null)
+                        {
+                            await this.ApproachEntity(blinkAbility, rune, checkRange);
+                        }
+
+                        this.owner.PickUpRune(rune);
+                        continue;
+                    }
                 }
             }
         }
--- Snatcher/SnatcherConfig.cs
+++ Snatcher/SnatcherConfig.cs
@@ -15,29 +15,46 @@ namespace Snatcher
     {
         private readonly MenuFactory factory;
 
+        //private readonly Dictionary<string, bool> greedDictionary = new Dictionary<string, bool>
+        //                                                                {
+        //                                                                    ["item_aegis"] = true,
+        //                                                                    ["item_cheese"] = true,
+        //                                                                    ["rune_bounty"] = false,
+        //                                                                    ["item_gem"] = true,
+        //                                                                    ["item_rapier"] = true,
+        //                                                                };
+
+        private readonly Dictionary<string, bool> snatchDictionary = new Dictionary<string, bool>
+                                                                         {
+                                                                             ["item_aegis"] = true,
+                                                                             ["item_cheese"] = true,
+                                                                             ["rune_bounty"] = true,
+                                                                             ["item_gem"] = true,
+                                                                             ["item_rapier"] = true,
+                                                                         };
+
         private bool disposed;
 
         public SnatcherConfig()
         {
             this.factory = MenuFactory.Create("Snatcher");
-            var dict = new Dictionary<string, bool>
-                           {
-                               { "item_aegis", true },
-                               { "item_cheese", true },
-                               { "rune_bounty", true },
-                               { "item_gem", true },
-                               { "item_rapier", true },
-                           };
-            this.ToggleHotkey = this.factory.Item("Toggle Hotkey", new KeyBind((uint)KeyInterop.VirtualKeyFromKey(Key.P), KeyBindType.Toggle));
-            this.HoldHotkey = this.factory.Item("Hold Hotkey", new KeyBind(0, KeyBindType.Press));
-            this.SnatchOptions = this.factory.Item("Snatch Options", new AbilityToggler(dict));
 
-            this.ScanIntervall = this.factory.Item("Scan Intervall", new Slider(125, 0, 1000));
+            this.ToggleHotkey = this.factory.Item("Toggle Hotkey", new KeyBind((uint)KeyInterop.VirtualKeyFromKey(Key.P), KeyBindType.Toggle));
+            this.HoldHotkey = this.factory.Item("Hold Hotkey", new KeyBind(0));
+            this.SnatchOptions = this.factory.Item("Snatch Options", new AbilityToggler(this.snatchDictionary));
+
+            this.ScanIntervall = this.factory.Item("Scan Intervall", new Slider(125, 1, 1000));
             this.ScanIntervall.Item.Tooltip = "How fast it scans for items to pickup.";
 
             this.SwapItem = this.factory.Item("Swap Item", false);
             this.SwapItem.Item.Tooltip = "Swaps the lowest cost item to the backpack when inventory full.";
+
+            // this.GreedMode = this.factory.Item("Greed Mode", new AbilityToggler(this.greedDictionary));
+            this.GreedMode = this.factory.Item("Greed Mode", false);
+            this.GreedMode.Item.Tooltip = "Uses blink dagger to reach items.";
         }
+
+        public MenuItem<bool> GreedMode { get; set; }
 
         public MenuItem<KeyBind> HoldHotkey { get; }
 
