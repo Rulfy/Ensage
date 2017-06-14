20c48e9231026b5538470fcc6097bfe831fc3af9
Rulfy <git@zynox.net>
2017-06-14 12:12:32 +0200

    added visible by enemy test app

---
+++ VisibleByEnemyTestApp/VisibleByEnemyTestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VisibleByEnemyTestApp
+{
+    using Ensage;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Entity.OnInt32PropertyChange += Entity_OnInt32PropertyChange;
+            Console.WriteLine("VisibleByEnemyTestApp loaded");
+        }
+
+        private static void Entity_OnInt32PropertyChange(Entity sender, Int32PropertyChangeEventArgs args)
+        {
+            var hero = sender as Hero;
+            if (args.PropertyName == "m_iTaggedAsVisibleByTeam" && hero != null)
+            {
+                var me = ObjectManager.LocalHero;
+
+                var output = $"{sender.Name}: {args.OldValue} => {args.NewValue} | IsVisibleToEnemies: {!hero.IsVisibleToEnemies} | OwnTeam: {hero.Team == me?.Team} | Me: {hero == me}";
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
